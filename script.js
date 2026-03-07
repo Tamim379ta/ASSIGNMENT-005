673527da30d4f735c7b8e6073df422d54243b6d6
Tamim379ta <tamimthasan55@gmail.com>
2026-03-07 16:36:13 +0600

    added open and closed function

--- script.js
+++ script.js
@@ -114,4 +114,115 @@ const displayModal = (data) => {
 }
 
 
+const loadOpenData = async() => {
+    manageLoading(true)
+  const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+  const response = await fetch(url)
+  const data = await response.json();
+
+  // console.log(data.data)  
+  displayOpenData(data.data)
+
+}
+
+const displayOpenData = (cards) => {
+  const CardContainer = document.getElementById("card-container")
+  const openCard = cards.filter(card => card.status == "open"); 
+  let totalIssues = document.getElementById("total-issues");
+  totalIssues.innerText = openCard.length;
+  console.log(openCard)  
+  CardContainer.innerHTML = ""; 
+  openCard.forEach(card => {
+    const eachCard = document.createElement("div");
+    eachCard.innerHTML = `
+   <div class=" h-70 shadow-sm rounded-[10px]  ${ card.status == "open"? "border-t-4 border-green-600" : "border-t-4 border-purple-600" }  bg-white">
+      <div class="flex justify-between p-3">
+        <div><img src="${card.status == "open" ? "./assets/Open-Status.png" : "./assets/Closed- Status .png"}" alt=""></div>
+        <div class="badge badge-outline badge-secondary">${card.priority}</div>
+      </div>
+      <div class="space-y-2 px-3">
+        <h1 onclick = "loadModal(${card.id})" class="font-bold">${card.title}</h1>
+        <p class="line-clamp-2">${card.description}</p>
+
+        <div class="flex gap-2 py-2">
+          <div class="badge badge-outline badge-error">${card.labels[0]}</div>
+          <div class="badge badge-outline badge-warning">${card.labels[1]? card.labels[1] : "Not Found" }</div>
+        </div>
+
+      </div>
+      <hr class="text-gray-300">
+      <div class="space-y-2 p-3">
+          <h1>${card.createdAt}</h1>
+          <p>${card.updatedAt}</p>
+      </div>
+    </div>
+  
+  `
+
+  CardContainer.appendChild(eachCard); 
+    
+  });
+
+  manageLoading(false)
+
+}
+
+
+const loadClosedData = async() => {
+    manageLoading(true)
+  const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+  const response = await fetch(url)
+  const data = await response.json();
+
+  // console.log(data.data)  
+  displayClosedData(data.data)
+
+}
+
+const displayClosedData = (cards) => {
+  const CardContainer = document.getElementById("card-container")
+  const openCard = cards.filter(card => card.status == "closed"); 
+  let totalIssues = document.getElementById("total-issues");
+  totalIssues.innerText = openCard.length;
+  console.log(openCard)  
+  CardContainer.innerHTML = ""; 
+  openCard.forEach(card => {
+    const eachCard = document.createElement("div");
+    eachCard.innerHTML = `
+   <div class=" h-70 shadow-sm rounded-[10px]  ${ card.status == "open"? "border-t-4 border-green-600" : "border-t-4 border-purple-600" }  bg-white">
+      <div class="flex justify-between p-3">
+        <div><img src="${card.status == "open" ? "./assets/Open-Status.png" : "./assets/Closed- Status .png"}" alt=""></div>
+        <div class="badge badge-outline badge-secondary">${card.priority}</div>
+      </div>
+      <div class="space-y-2 px-3">
+        <h1 onclick = "loadModal(${card.id})" class="font-bold">${card.title}</h1>
+        <p class="line-clamp-2">${card.description}</p>
+
+        <div class="flex gap-2 py-2">
+          <div class="badge badge-outline badge-error">${card.labels[0]}</div>
+          <div class="badge badge-outline badge-warning">${card.labels[1]? card.labels[1] : "Not Found" }</div>
+        </div>
+
+      </div>
+      <hr class="text-gray-300">
+      <div class="space-y-2 p-3">
+          <h1>${card.createdAt}</h1>
+          <p>${card.updatedAt}</p>
+      </div>
+    </div>
+  
+  `
+
+  CardContainer.appendChild(eachCard); 
+    
+  });
+
+  manageLoading(false)
+
+}
+
+
+
 loadData()
